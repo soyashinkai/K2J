2c02fc3357fa6628dcf402f60b39e03091e55371
Soya SHINKAI <soya.shinkai@gmail.com>
2020-09-08 11:33:47 +0900

    initial commit

--- README.md
+++ README.md
@@ -6,3 +6,27 @@ The theoretical background and demonstration are described in:
 - Soya Shinkai, Takeshi Sugawara, Hisashi Miura, Ichiro Hiratani, and Shuichi Onami. (2020). **Microrheology for Hi-C Data Reveals the Spectrum of the Dynamic 3D Genome Organization.** [_Biophysical Journal_ **118** 2220–2228](https://doi.org/10.1016/j.bpj.2020.02.020).
 
 - Soya Shinkai, Shuichi Onami, and Ryuichiro Nakato. (2020). **Toward understanding the dynamic state of 3D genome.** [_Computational and Structural Biotechnology Journal_ **18** 2259–2269](https://doi.org/10.1016/j.csbj.2020.08.014).
+
+## Requirements
+
+K2J codes require the following Python libraries:
+
+-   os
+-   sys
+-   numpy
+-   matplotlib
+
+## Quick Start
+
+Move to the directory [_Codes_](/Codes):
+
+    cd Codes
+
+Then, run the following scripts:
+
+    ./run_1.sh
+    ./run_2.sh
+    ./run_3.sh
+    ./run_4.sh
+
+These scripts generate spectra data in the directory [_data_J_tan_](/data_J_tan) and figures in the directory [_figs_](/figs).
---
+++ Tutorial/2_plot_Js.py
@@ -0,0 +1,60 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+import os
+# --------------------------------------------------------------------------------------------------
+# Set command line arguments
+argv = sys.argv
+argc = len(argv)
+if (argc != 4):
+    print("Usage: python " + argv[0] + " K_FILE UPPER LOWER")
+    exit()
+K_FILE = argv[1]
+UPPER = int(argv[2])  # the exponent of the upper ω
+LOWER = int(argv[3])  # the exponent of the lower ω
+# --------------------------------------------------------------------------------------------------
+K = np.loadtxt(K_FILE)
+N = K.shape[0]
+DIR = "data_J_tan"
+os.makedirs("figs", exist_ok=True)
+FILE_FIG = "figs/Js.png"
+# --------------------------------------------------------------------------------------------------
+
+
+def main():
+    # ----------------------------------------------------------------------------------------------
+    plt.rcParams["font.family"] = "Arial"
+    plt.rcParams["font.size"] = 36
+    # ----------------------------------------------------------------------------------------------
+    plt.figure(figsize=(12, 9))
+    plt.xscale("log")
+    plt.yscale("log")
+    plt.xlim(10**(LOWER), 10**(UPPER))
+    plt.ylim(1e-2, 1e3)
+    plt.xlabel(r"$\mathrm{\mathbf{\bar{\omega}}}$")
+    plt.ylabel(r"$\mathrm{\mathbf{Normalized\ compliance}}$")
+
+    for n in range(N):
+        FILE_READ = DIR + "/J_tan_n{0:d}.txt".format(n)
+        data = np.loadtxt(FILE_READ)
+        plt.plot(data[:, 0], data[:, 1], linewidth=1, color="blue", alpha=0.1)
+        plt.plot(data[:, 0], data[:, 2], linewidth=1, color="red", alpha=0.1)
+
+    plt.annotate(r"$\mathrm{\mathbf{\bar{J}'(\bar{\omega})\ (storage)}}$",
+                 (0.005, 500), color="blue")
+    plt.annotate(r"$\mathrm{\mathbf{\bar{J}''(\bar{\omega})\ (loss)}}$",
+                 (0.005, 100), color="red")
+
+    plt.gca().spines["right"].set_visible(False)
+    plt.gca().spines["top"].set_visible(False)
+    plt.gca().yaxis.set_ticks_position("left")
+    plt.gca().xaxis.set_ticks_position("bottom")
+
+    plt.tight_layout()
+    plt.savefig(FILE_FIG)
+    plt.close()
+# --------------------------------------------------------------------------------------------------
+
+
+if __name__ == '__main__':
+    main()
